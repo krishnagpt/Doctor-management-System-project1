82f995ab0bcf6b622ceaa2c73328d65fd0bb44d6
krishngu <krishngu@KRISHNGU01.corp.amdocs.com>
2023-09-25 23:48:01 +0530

    commit doctor management system project

---
+++ src/com/amdocs/doctormanagement/exception/DoctorNotFoundException.java
@@ -0,0 +1,21 @@
+package com.amdocs.doctormanagement.exception;
+
+public class DoctorNotFoundException extends Exception{
+	
+	private String message;
+	
+	public DoctorNotFoundException()
+	{}
+	
+	public DoctorNotFoundException(String message)
+	{
+		this.message=message;
+	}
+
+	@Override
+	public String toString() {
+		return "DoctorNotFoundException [message=" + message + "]";
+	}
+
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module DoctorManagementSystem1 {
+}
